c0ad6cc0d835b9d18d7fb3bb5317bb6298dfa07a
Noahaxel <126935789+Noahaxel@users.noreply.github.com>
2024-04-27 21:37:17 +0800

    Update data_reader.py

--- GAN/utils/data_reader.py
+++ GAN/utils/data_reader.py
@@ -3,15 +3,15 @@ from torchvision.transforms import transforms
 from PIL import Image
 import os
 
-imgsz=512  #缩放图像的大小
+imgsz=128  #缩放图像的大小
 
 #定义数据读取器
 class LoadData(Dataset):
     def __init__(self, dir_path, number_of_channels):
-        self.imgs_info = [os.path.join(dir_path,img) for img in os.listdir(dir_path)]
+        self.imgs_info = [(os.path.join(dir_path,img),dir_path[-1]) for img in os.listdir(dir_path)]
 
         self.tf = transforms.Compose([
-            # 将图片尺寸resize到512*512
+            # 将图片尺寸resize到128*128
             transforms.Resize((imgsz,imgsz)),
             # 将图片转化为Tensor格式
             transforms.ToTensor(),
@@ -22,7 +22,7 @@ class LoadData(Dataset):
             ])
 
     def __getitem__(self, index):
-        img_path = self.imgs_info[index]
+        img_path, label = self.imgs_info[index]
         img = Image.open(img_path)
         img = img.convert('RGB')
         img = self.tf(img)
